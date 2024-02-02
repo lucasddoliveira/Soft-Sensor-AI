9c0fb4e43f56d35129afd4997e14b5b3da13e815
Lucas Dantas <lucasddoliveira1@gmail.com>
2024-02-02 14:57:36 -0300

    ...

--- CollectAndSendData/getSensorsData.py
+++ CollectAndSendData/getSensorsData.py
@@ -2,30 +2,74 @@ import paho.mqtt.client as mqtt
 import json
 from datetime import datetime, timedelta
 import pytz
+import mysql.connector
 import time
 import threading
 import pandas as pd
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MQTT_BROKER = os.getenv('MQTT_BROKER')
+MQTT_TOPIC = os.getenv('MQTT_TOPIC')
+MQTT_PORT = int(os.getenv('MQTT_PORT'))
+MQTT_USERNAME = os.getenv('MQTT_USERNAME')
+MQTT_PASSWORD = os.getenv('MQTT_PASSWORD')
+
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+MYSQL_USERNAME = os.getenv('MYSQL_USERNAME')
+MYSQL_URI = os.getenv('MYSQL_URI')
+MYSQL_TABLE = os.getenv('MYSQL_URI')
+MYSQL_DATABASE = os.getenv('MYSQL_URI')
 
 counter = 0
-leitura = []
-df = pd.DataFrame(columns=["Variable 1", "Variable 2", "Variable 3", "Variable 4", "Variable 5"])
+leitura = [0,0,0,0,0,0]
+df = pd.DataFrame(columns=["DP_995796", "DP_564065", "DP_035903", "DP_012072", "DP_862640"])
 
 def on_message(client, userdata, message):
     global counter, leitura, df  # Add these lines to declare counter, leitura, and df as global variables
     payload = message.payload.decode('utf-8')
-    
+
     try:
         if counter < 5:
-            leitura.append(float(payload))  # This will print the parsed JSON data
+            if('DP_995796' in message.topic):
+                leitura[1]= float(payload)  # This will print the parsed JSON data
+            elif('DP_564065' in message.topic):
+                leitura[2]= float(payload)
+            elif('DP_035903' in message.topic):
+                leitura[3]= float(payload)
+            elif('DP_012072' in message.topic):
+                leitura[4]= float(payload)
+            elif('DP_862640' in message.topic):
+                leitura[5]= float(payload)
+            else:
+                return
 
         if counter == 4:
-            new_df = pd.DataFrame([leitura], columns=df.columns)
-            df = pd.concat([df, new_df], ignore_index=True)
-    
+            fuso_horario = pytz.timezone('America/Sao_Paulo')
+            hora_atual = datetime.now(fuso_horario)
+            leitura[0] = hora_atual
+
+            cnx = mysql.connector.connect(
+                host=MYSQL_URI,
+                user=MYSQL_USERNAME,
+                password=MYSQL_PASSWORD,
+                database=MYSQL_DATABASE
+            )
+
+            cursor = cnx.cursor()
+            cursor.execute('INSERT INTO '+str(MYSQL_TABLE)+' (timestamp, DP_995796, DP_564065, DP_035903, DP_012072, DP_862640) VALUES (%s, %s, %s, %s, %s, %s)', leitura)
+
+
+            cnx.commit()
+            cursor.close()
+            cnx.close()
+
             counter = 0
-            leitura = []
-            
-            print(df)
+            leitura = [0,0,0,0,0,0]
+
+
             return
 
         counter += 1
@@ -35,7 +79,6 @@ def on_message(client, userdata, message):
 
 def on_disconnect(client, userdata, rc):
     print("Conexão perdida. Tentando reconectar...")
-    print(userdata)
     print(rc)
     time.sleep(5)  # Aguarda 5 segundos antes de tentar reconectar
     client.reconnect()
@@ -45,16 +88,11 @@ def alert():
         print('SERVER ON')
         time.sleep(20)
 
-# Configurações do broker MQTT
-mqtt_server = "mqtt.lab.cagepa.pb.gov.br"
-mqtt_port = 2884
-mqtt_topic = "ufpbcear/#"
-
 # Criação de um cliente MQTT
 client = mqtt.Client()
 client.on_message = on_message
 client.on_disconnect = on_disconnect
-client.username_pw_set("ufpbcear", "Ufpb@C3ar23")
+client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
 
 #thread = threading.Thread(target=alert)
 #thread.daemon = True  # Define a thread como um daemon para encerrar junto com o programa principal
@@ -64,8 +102,8 @@ client.username_pw_set("ufpbcear", "Ufpb@C3ar23")
 while True:
     try:
         # Conecta-se ao broker MQTT
-        client.connect(mqtt_server, port=mqtt_port)
-        client.subscribe(mqtt_topic)
+        client.connect(MQTT_BROKER, port= MQTT_PORT)
+        client.subscribe(MQTT_TOPIC)
         # Mantém a conexão e lida com as mensagens recebidas
         client.loop_forever()
     except KeyboardInterrupt:
